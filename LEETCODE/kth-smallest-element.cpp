b36425f21946558cb46a113ee0915717e6f255ef
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2022-10-10 00:07:14 +0500

    find the kth-smallest element in BST

---
+++ LEETCODE/kth-smallest-element.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int> temp;
+        TreeNode* node; 
+        stack<TreeNode *> s1;
+        s1.push(root);
+        while(!s1.empty())
+        {
+            TreeNode * curr = s1.top();
+            int val=curr->val;
+            temp.push_back(val);
+            s1.pop();
+            if(curr->left != NULL)
+                s1.push(curr->left);
+            if(curr->right != NULL)
+                s1.push(curr->right);
+        }
+        sort(temp.begin(),temp.end());
+        return temp.at(k-1);
+    }
+};
