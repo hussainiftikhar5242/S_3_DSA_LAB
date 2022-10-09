ac10a4adf9bf8c3d9234ebd010ba694297b9afa2
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2022-10-10 00:07:51 +0500

    Insert a element in BST

---
+++ LEETCODE/insertion-in-BST.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        TreeNode* node=root;
+        TreeNode* temp=NULL;
+        while(node != NULL)
+        {
+            temp=node;
+            if(node->val > val)
+            {
+                node=node->left;
+            }
+            else
+            {
+                node=node->right;
+            }
+        }
+        if(node == NULL)
+        {
+            temp->left=new TreeNode(val);
+        }
+        return node;
+        
+    }
+};
